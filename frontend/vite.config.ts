ac01c06275172b58b9540075e1270fe113190a8f
William Ku <jrhykushii@gmail.com>
2024-12-27 15:33:12 -0500

    Adds node types

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // This is needed for Docker
+    watch: {
+      usePolling: true, // This is important for Docker on some systems
+    },
+  },
+});
